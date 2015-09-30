1a9410fcefbf53493c83073d70a65940945a8878
Cuauhtemoc Moreno <oddgoo@gmail.com>
2015-10-01 00:47:56 +1000

    -Swapped Google Forms out, added Parse.com form system. -Organised CSS, added Error/thank you rules -Added form with Vanilla controls.

---
+++ js/main.js
@@ -0,0 +1,49 @@
+
+$( document ).ready(function() {
+
+    $('input, textarea').placeholder();
+
+    var parseAPPID = "tSb3VndkOZyiSPdiQ2H8ZGW9VcG1MkcrDUq5aIfi";
+  	var parseJSID = "inJFPPATJeCU5WaePQMJty4vZ7b5bCPDLT9Vrx3u";
+  	Parse.initialize(parseAPPID, parseJSID);
+
+    // ========= Form submit
+  	$("#entry-form").on("submit", function(e) {
+
+  		e.preventDefault();
+      $(".error").hide();
+  		//add error handling here
+  		//gather the form data
+
+  		var data = {};
+  		data.invitee_name = $("#form-name").val();
+      data.guests = $("#form-guests").val();
+  		data.contact = $("#form-contact").val();
+      data.comments = $("#form-comments").val();
+      // ========= Validation ==============
+      showMessage("Sending...");
+
+      // ========= Sending to server =======
+
+      var EntryObject = Parse.Object.extend("Entry");
+      var entry = new EntryObject();
+
+      entry.save(data, {
+        success:function() {
+          $("#thank-you").show();
+  				$("#entry-form").hide();
+          $(document).scrollTop( $("#thank-you").offset().top );
+        },
+        error:function(request, error) {
+          console.log(error);
+          showMessage(error.message);
+        }
+      });
+    });
+});
+
+function showMessage(error){
+  $(document).scrollTop( $('.error').offset().top );
+  $('.error').show();
+  $('.error').html(error);
+}
